57731fc84552392a7e8ba9d1666fbbc634b1d7c2
Two J <TwoJ@gmail.com>
2023-06-26 00:35:13 +0630

    auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,46 +9,47 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export const authOptions: AuthOptions = {
   providers: [
-    GithubProvider({
-      clientId: config.githubClientId,
-      clientSecret: config.githubClientSecret,
-    }),
+    process.env.VERCEL_ENV === "preview"
+      ? Credentials({
+          id: "credentials",
+          name: "Credentials",
+          credentials: {
+            email: {
+              label: "Email",
+              type: "text",
+            },
+            password: {
+              label: "Password",
+              type: "password",
+            },
+          },
+          async authorize(credentials) {
+            if (!credentials?.email || !credentials.password) {
+              throw new Error("Email and Password required!");
+            }
+            const user = await prismadb.user.findUnique({
+              where: { email: credentials.email },
+            });
+            if (!user || !user.hashedPassword) {
+              throw new Error("Email does not exist!");
+            }
+            const isCorrectPassword = await compare(
+              credentials.password,
+              user.hashedPassword
+            );
+            if (!isCorrectPassword) {
+              throw new Error("InCorrect password!");
+            }
+            return user;
+          },
+        })
+      : GithubProvider({
+          clientId: config.githubClientId,
+          clientSecret: config.githubClientSecret,
+        }),
     GoogleProvider({
       clientId: config.googleClientId,
       clientSecret: config.googleClientSecret,
-    }),
-    Credentials({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "text",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-        },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials.password) {
-          throw new Error("Email and Password required!");
-        }
-        const user = await prismadb.user.findUnique({
-          where: { email: credentials.email },
-        });
-        if (!user || !user.hashedPassword) {
-          throw new Error("Email does not exist!");
-        }
-        const isCorrectPassword = await compare(
-          credentials.password,
-          user.hashedPassword
-        );
-        if (!isCorrectPassword) {
-          throw new Error("InCorrect password!");
-        }
-        return user;
-      },
     }),
   ],
   pages: {
